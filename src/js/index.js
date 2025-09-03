d49231578a1b58efd191b11853206a3c16106b8f
Yan <yanbocharov.tech@yandex.ru>
2025-09-03 19:47:19 +0300

    layout done!

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,27 @@
 // Галерея и лайтбоксы от Fancybox
-import { Fancybox } from '@fancyapps/ui';
-import '@fancyapps/ui/dist/fancybox/fancybox.css';
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
 
-Fancybox.bind('[data-fancybox]', {
-	// Your custom options
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
 });
 
 // Мобильная навигация
-import mobileNav from './modules/mobile-nav.js';
+import mobileNav from "./modules/mobile-nav.js";
 mobileNav();
+
+/* DARK MODE */
+
+const darkModeBtns = document.querySelectorAll(".toggleDarkModeBtn");
+
+darkModeBtns.forEach((btn) => btn.addEventListener("click", toggleDarkMode));
+
+function toggleDarkMode(e) {
+  const darkMode = document.body.classList.toggle("dark");
+
+  if (darkMode) {
+    e.target.innerText = "☀️";
+  } else {
+    e.target.innerText = "🌘";
+  }
+}
